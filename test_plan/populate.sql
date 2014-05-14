2321edb8e008eb407fa5a86ffc76f601195dc749
Andrew Zschorn <andrew.zschorn@precisionpc.com.au>
2014-05-14 23:26:48 +0930

    more testing things

--- test_plan/populate.sql
+++ test_plan/populate.sql
@@ -1,49 +1,51 @@
+-- IDs
+--   Applicant 1-9-
+--   Application x11 - x19
+--   Degree      x21 - x29
+--   Document    x31 - x39
+
 --------------------------------------------------------------------------------
 -- Application for PhD studies.txt
+-- 01
 
-INSERT INTO Applicant (FName, LName, Email, Mobile, StreetAddress, City,
-Postcode, AddressCountryISOCode)
-VALUES ('Shirin', 'Ebadi', 'shirin.ebadi@keb.com.de', '+49 (176) 6488 9999',
-'Zwillingstr 99, App 099', 'Munich', 80807, 'DE');
--- capture  LAST_INSERT_ID() as se_applicant_id 
+INSERT INTO Applicant (ApplicantID, FName, LName, Email, Mobile, StreetAddress, 
+City, Postcode, AddressCountryISOCode)
+VALUES (01, 'Shirin', 'Ebadi', 'shirin.ebadi@keb.com.de', 
+'+49 (176) 6488 9999', 'Zwillingstr 99, App 099', 'Munich', 80807, 'DE');
 
-
-INSERT INTO Degree (ApplicantID, Name, Type, YearCompleted, InstitutionName,
-InstitutionCountryCode) 
-VALUES (se_applicant_id, 'Electrical Engineering with expertise in Control
+INSERT INTO Degree (DegID, ApplicantID, Name, Type, YearCompleted, 
+InstitutionName, InstitutionCountryCode) 
+VALUES (121, 01, 'Electrical Engineering with expertise in Control
 Theory and Reat-Time Applications', 'bachelor', 2005, 'University of Tabriz',
 'IR') ;
 
-
 INSERT INTO Degree (ApplicantID, Name, Type, YearCompleted, InstitutionName,
 InstitutionCountryCode) 
-VALUES (se_applicant_id, 'Electrical Engineering with expertise in Control
+VALUES (122, 'Electrical Engineering with expertise in Control
 Theory and Reat-Time Applications', 'masters', 2008, 'University of Tabriz',
 'IR') ;
 
-INSERT INTO Application (ApplicantID, awardType)
-VALUES (se_applicant_id, 'PhD') ;
+INSERT INTO Application (ApplicationID, ApplicantID, awardType)
+VALUES (111, 'PhD') ;
 
-INSERT INTO Document (UploadLink, ApplicantID, DocType)
-VALUES ('/mnt/data/rhd/' + se_applicant_id + '/CV/001.pdf', se_applicant_id,
-'CV' );
+INSERT INTO Document (DocID, UploadLink, ApplicantID, DocType)
+VALUES (131, '/mnt/data/rhd/01/CV/001.pdf', 01, 'CV' );
 
 
 --------------------------------------------------------------------------------
 -- FW Your kind supervision for my intended PhD.txt
+-- 2
 -- TODO: 2 emails (also memalki@uqu.edu.sa)
-INSERT INTO Applicant(FName, LName, Email, Mobile, Phone)
+INSERT INTO Applicant(2, FName, LName, Email, Mobile, Phone)
 VALUES ('Mohammad', 'Almalki', 'don.memo@hotmail.com', '+966 565907070', 
 '+966 12 527000000 ext 4951');
--- capture  LAST_INSERT_ID() as ma_applicant_id 
 
-INSERT INTO Application (ApplicantID, awardType)
-VALUES (ma_applicant_id, 'PhD') ;
--- capture LAST_INSERT_ID() as ma_application_id
+INSERT INTO Application (ApplicantID, ApplicationID, awardType)
+VALUES (2, 211, 'PhD') ;
 
-INSERT INTO Degree (ApplicantID, Name, Type, YearCompleted, InstitutionName,
-InstitutionCountryCode) 
-VALUES (ma_applicant_id, 'IT', 'master', 2010,
+INSERT INTO Degree (DegID, ApplicantID, Name, Type, YearCompleted, 
+InstitutionName, InstitutionCountryCode) 
+VALUES (221, 2, 'IT', 'master', 2010,
 'University of Technology Sydney', 'AU') ;
 
 -- TODO: how to capture 'e-government in higher education organizations' ?
@@ -69,6 +71,7 @@ ma_applicant_id, ma_application_id, 'proposal') ;
 
 --------------------------------------------------------------------------------
 -- Fwd Flinders Application - PhD (Comp Sc) - Sem 2 2015.txt
+-- 3
 INSERT INTO Applicant(FName, LName, StudentID)
 VALUES ('Azzam', 'Alwash', '1234567') ;
 -- capture LAST_INSERT_ID() as aa_applicant_id
@@ -91,6 +94,7 @@ aa_applicant_id, aa_application_id, 'application');
 
 --------------------------------------------------------------------------------
 -- Fwd Flinders Application - PhD (Computer Science) Sem 2 2014 .txt
+-- 4
 INSERT INTO Applicant(FName, LName, StudentID)
 VALUES ('Mustafa', 'Al Lami', 2130106);
 -- capture LAST_INSERT_ID() as mal_applicant_id
@@ -120,6 +124,7 @@ mal_applicant_id, 'general');
 
 --------------------------------------------------------------------------------
 -- Fwd Flinders Application.txt
+-- 5
 INSERT INTO Applicant(FName, StudentID)
 VALUES ('Ena', '2345678') ;
 -- capture LAST_INSERT_ID() as ena_applicant_id
@@ -164,6 +169,7 @@ VALUES ('/mnt/data/rhd/' + ena_applicant_id +
 
 --------------------------------------------------------------------------------
 -- Fwd PhD inquiry.txt
+-- 6
 INSERT INTO Applicant (FName, LName, Sex)
 VALUES ('Fakhri', 'Bazzaz', 'M') ;
 -- capture LAST_INSERT_ID() as fb_applicant_id
@@ -197,6 +203,10 @@ INSERT INTO Document (UploadLink, ApplicantID, DocType, Description)
 VALUES ('/mnt/data/rhd/' + fb_applicant_id + '/publication/0001.pdf',
 fb_applicant_id, 'publication', 'masters thesis');
 
+-- INSERT INTO Publications (UploadLink, ApplicantID, DocType, Description)
+-- VALUES ('/mnt/data/rhd/' + fb_applicant_id + '/publication/0001.pdf',
+-- fb_applicant_id, 'publication', 'masters thesis');
+
 INSERT INTO Document (UploadLink, ApplicantID, DocType, Description)
 VALUES ('/mnt/data/rhd/' + fb_applicant_id + '/transcript/0001.pdf',
 fb_applicant_id, 'transcript', 'masters transcript');
@@ -210,8 +220,56 @@ VALUES ('/mnt/data/rhd/' + fb_applicant_id + '/reference/0002.pdf',
 fb_applicant_id, fb_application_id, 'reference') ;
 
 
+--------------------------------------------------------------------------------
 -- Fwd Requesting for PhD supervision.txt
+-- 7
+INSERT INTO Applicant (FName)
+VALUES ('Nemo') ;
+-- capture LAST_INSERT_ID() as nemo_applicant_id
+
+INSERT INTO Degree (ApplicantID, Name, Type, InstitutionName, 
+InstitutionCountryCode)
+VALUES (aa_applicant_id, 'IT', 'bachelors', 'Manipal University', 'IN') ;
+
+INSERT INTO Degree (ApplicantID, Name, Type, InstitutionName,
+InstitutionCountryCode)
+VALUES (aa_applicant_id, 'Software development and engineering',
+'masters', 'West Bengal University', 'IN') ;
+
+INSERT INTO Application (ApplicantID, Type, Email)
+VALUE (nemo_applicant_id, 'PhD', 'nemo@gmail.com') ; 
+
+INSERT INTO Document (UploadLink, ApplicantID, DocType)
+VALUES ('/mnt/data/rhd/' + nemo_applicant_id + '/resume/0001.pdf',
+nemo_applicant_id, 'resume');
+
+INSERT INTO Document (UploadLink, ApplicantID, DocType, Description)
+VALUES ('/mnt/data/rhd/' + nemo_applicant_id + '/publication/0001.pptx',
+nemo_applicant_id, 'publication', 
+'Six-plus presenting a six-element framework for sentiment analysis.pptx');
+
+INSERT INTO Document (UploadLink, ApplicantID, DocType, Description)
+VALUES ('/mnt/data/rhd/' + nemo_applicant_id + '/publication/0002.docx',
+nemo_applicant_id, 'publication', 
+'The price prediction framework based upon representation of the opinions');
+
+INSERT INTO Document (UploadLink, ApplicantID, DocType, Description)
+VALUES ('/mnt/data/rhd/' + nemo_applicant_id + '/publication/0003.docx',
+nemo_applicant_id, 'publication', 
+'The classification framework for the representation of opinions.docx');
+
+--------------------------------------------------------------------------------
 -- PhD Student.txt
+-- 8
+INSERT INTO Applicant(Fname, Email)
+VALUES ('Sara', 'sara@gmail.com') ;
+-- capture LAST_INSERT_ID() as sara_applicant_id
+
+INSERT INTO Degree (ApplicantID, Name, Type, YearCompleted, GPA,
+InstitutionName, InstitutionCountryCode)
+VALUES 
+
+
 -- PhD Student1.txt
 -- PhD Student2.txt
 -- RHD Admission form - march2014.docx
