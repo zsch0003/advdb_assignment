39e1487d6491847b3968558f5682306b0a781cc3
Andrew Zschorn <andrew.zschorn@precisionpc.com.au>
2014-05-31 08:26:25 +0930

    file permissions mode change under linux

--- test_plan/create_views.sql
+++ test_plan/create_views.sql
@@ -1,29 +1,13 @@
 -- -----------------------------------------------------------------------------
 -- For a member of staff, create a view that's shows all the ongoing
--- applications that they're in some way responsible for.
+-- applications that they're in some way working on.
+--
+-- In the following, we have hard-coded a user/staff ID of 1000, as we don't
+-- have permission in the CSEM MySQL server to create database users. In a real
+-- system, we would replace the hard-coded values with the function USER() to
+-- get the current database user ID.
 
--- DROP VIEW IF EXISTS `Overview for staff`;
--- CREATE VIEW `Overview for staff` AS
--- SELECT appt.FName, appt.LName, appt.Email, app.DateLastModified, 
--- AppStat.Status
--- FROM Applicant appt
--- INNER JOIN Application app ON  appt.ApplicantID = app.ApplicantID 
--- INNER JOIN `Application Status` AppStat 
---   ON  AppStat.ApplicationStatusID = app.applicationStatusID
--- WHERE appt.LastModifiedByStaffID = 1000 
-
--- UNION 
-
--- SELECT appt.FName, appt.LName, appt.Email, app.DateLastModified, 
--- AppStat.Status
--- FROM Applicant appt
--- INNER JOIN Application app ON  appt.ApplicantID = app.ApplicantID 
--- INNER JOIN `Application Status` AppStat 
---   ON  AppStat.ApplicationStatusID = app.applicationStatusID
--- INNER JOIN `Supervise as` super ON super.ApplicationID = app.ApplicationID
--- WHERE super.StaffID = 1000 
--- ;
-
+-- A utility view with lookup values cached out
 DROP VIEW IF EXISTS Application_Expanded ;
 CREATE VIEW Application_Expanded AS
 SELECT Appt.FName, Appt.LName, Appt.Email, App.*, AppStat.Status 
@@ -34,6 +18,7 @@ INNER JOIN `Application Status` AppStat
   ON AppStat.ApplicationStatusID = App.applicationStatusID
 ; 
 
+-- All the Applications that the current user is marked as supervising
 DROP VIEW IF EXISTS `MyRHDApps_SupervisedByMe_Expanded` ;
 CREATE VIEW `MyRHDApps_SupervisedByMe_Expanded` AS
 SELECT App.* 
@@ -42,6 +27,7 @@ INNER JOIN `Supervise as` Super ON Super.ApplicationID = App.ApplicationID
 WHERE Super.StaffID = 1000 
 ;
 
+-- All the Applications that the current user has flagged
 DROP VIEW IF EXISTS `MyRHDApps_FlaggedByMe_Expanded` ;
 CREATE VIEW `MyRHDApps_FlaggedByMe_Expanded` AS
 SELECT App.*
@@ -51,6 +37,7 @@ INNER JOIN `University Staff Member_Application` Flag
 WHERE Flag.StaffID = 1000 
 ;
 
+-- All the Applications that the current user has most recently modified
 DROP VIEW IF EXISTS MyRHDApps_LastModifiedByMe_Expanded ;
 CREATE VIEW MyRHDApps_LastModifiedByMe_Expanded AS
 SELECT App.*
@@ -58,6 +45,7 @@ FROM Application_Expanded App
 WHERE App.LastModifiedByStaffID = 1000 
 ;
 
+-- Composite view of the above
 DROP VIEW IF EXISTS `MyRHDApps_Expanded` ;
 CREATE VIEW `MyRHDApps_Expanded` AS
 SELECT * FROM MyRHDApps_SupervisedByMe_Expanded
@@ -67,12 +55,9 @@ UNION
 SELECT * FROM MyRHDApps_LastModifiedByMe_Expanded
 ;
 
+-- As above, but with irrelevent columns suppressed
 DROP VIEW IF EXISTS `MyRHDApps` ;
 CREATE VIEW `MyRHDApps` AS
 SELECT FName, LName, Email, Status FROM MyRHDApps_Expanded
 
--- CREATE VIEW `Overview for staff` AS
--- SELECT appt.FName, appt.LName, appt.Email 
--- FROM Application app
--- INNER JOIN Applicant appt ON appt.ApplicantID = app.ApplicantID AND Application.LastModifiedByStaffID = 1000;
 
