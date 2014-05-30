cbc47be7c10c3321eaf8039efcb1258088961405
Andrew Zschorn <andrew.zschorn@precisionpc.com.au>
2014-05-30 15:50:34 +0930

    starting some proper views

--- test_plan/create_views.sql
+++ test_plan/create_views.sql
@@ -1,6 +1,20 @@
 -- -----------------------------------------------------------------------------
--- create a view of the `supervies as` relation
+-- For a member of staff, create a view that's shows all the ongoing
+-- applications that they're in some way responsible for.
 
-CREATE VIEW `Supervise As Read` AS
-SELECT * FROM `Supervise as`;
+DROP VIEW IF EXISTS `Overview for staff`;
+
+CREATE VIEW `Overview for staff` AS
+SELECT appt.FName, appt.LName, appt.Email, app.DateLastModified, 
+AppStat.Status
+FROM Applicant appt
+INNER JOIN Application app ON  appt.ApplicantID = app.ApplicantID 
+INNER JOIN `Application Status` AppStat 
+  ON  AppStat.ApplicationStatusID = app.applicationStatusID
+WHERE appt.LastModifiedByStaffID = 1003 ;
+
+-- CREATE VIEW `Overview for staff` AS
+-- SELECT appt.FName, appt.LName, appt.Email 
+-- FROM Application app
+-- INNER JOIN Applicant appt ON appt.ApplicantID = app.ApplicantID AND Application.LastModifiedByStaffID = 1000;
 
