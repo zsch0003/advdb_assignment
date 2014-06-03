92f1571b0a609f4c99e9909ce8099dba14e5aa80
Andrew Zschorn <andrew.zschorn@precisionpc.com.au>
2014-06-03 18:24:43 +0930

    added lastmodifiedby names

--- test_plan/create_views_rhd_staff.sql
+++ test_plan/create_views_rhd_staff.sql
@@ -9,7 +9,9 @@ SELECT
   AppExpand.Email, 
   PrimaryUSM.FName AS PrimarySupervisorFName,
   PrimaryUSM.LName AS PrimarySupervisorLName,
-  COUNT(AssociateSuper.StaffID) AS `Associate supervisor count`
+  COUNT(AssociateSuper.StaffID) AS `Associate supervisor count`,
+  LastModifiedByUSM.FName AS LastModifiedByFName,
+  LastModifiedByUSM.LName AS LastModifiedByLName
 FROM Application_Expanded AS AppExpand
 LEFT JOIN (`Supervise as primary` AS PrimarySuper, 
            `University Staff Member` AS PrimaryUSM)
@@ -17,4 +19,6 @@ LEFT JOIN (`Supervise as primary` AS PrimarySuper,
       AND PrimarySuper.StaffID = PrimaryUSM.StaffID)
 LEFT JOIN (`Supervise as associate` AS AssociateSuper)
   ON (AppExpand.ApplicationID = AssociateSuper.ApplicationID)
+LEFT JOIN (`University Staff Member` AS LastModifiedByUSM)
+  ON (AppExpand.LastModifiedByStaffID = LastModifiedByUSM.StaffID)
 GROUP BY AppExpand.ApplicationID ;
