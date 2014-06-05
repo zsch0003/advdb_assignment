7f9e511b66dc982cb063e636b04e7e9534219823
Andrew Zschorn <zsch0003@flinders.edu.au>
2014-06-05 15:36:10 +0930

    made a separate Application_Expanded view

--- sql/create_views_utility.sql
+++ sql/create_views_utility.sql
@@ -1,16 +1,20 @@
 SELECT 'Creating utility views';
 
 -- A utility view with lookup values cached out
-DROP VIEW IF EXISTS Application_Ongoing_Expanded ;
-CREATE VIEW Application_Ongoing_Expanded AS
+DROP VIEW IF EXISTS Application_Expanded ;
+CREATE VIEW Application_Expanded AS
 SELECT Appt.FName, Appt.LName, Appt.Email, App.*, AppStat.Status 
 FROM Application App
 INNER JOIN Applicant Appt
   ON Appt.ApplicantID = App.ApplicantID
 INNER JOIN `Application Status` AppStat 
-  ON AppStat.ApplicationStatusID = App.applicationStatusID
-WHERE AppStat.Status LIKE 'ongoing%'; 
+  ON AppStat.ApplicationStatusID = App.applicationStatusID;
 
+DROP VIEW IF EXISTS Application_Ongoing_Expanded ;
+CREATE VIEW Application_Ongoing_Expanded AS
+SELECT *
+FROM Application_Expanded App
+WHERE App.Status LIKE 'ongoing%'; 
 
 -- Create views to separate primary supervisors from associate
 DROP VIEW IF EXISTS `Supervise as primary` ;
